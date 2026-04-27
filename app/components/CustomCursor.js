b1e6cb03628e2d95ab6e968ffabd522e723c7af3
Joazhan <joannzhang4@gmail.com>
2026-04-26 22:58:01 -0700

    CustomCursor: resize to 5x5px

--- app/components/CustomCursor.js
+++ app/components/CustomCursor.js
@@ -25,7 +25,7 @@ export default function CustomCursor() {
 
     const animate = () => {
       if (cursorRef.current) {
-        cursorRef.current.style.transform = `translate(${pos.current.x - 12}px, ${pos.current.y - 12}px)`
+        cursorRef.current.style.transform = `translate(${pos.current.x - 2.5}px, ${pos.current.y - 2.5}px)`
       }
       raf.current = requestAnimationFrame(animate)
     }
@@ -45,8 +45,8 @@ export default function CustomCursor() {
         position: 'fixed',
         top: 0,
         left: 0,
-        width: '24px',
-        height: '24px',
+        width: '5px',
+        height: '5px',
         borderRadius: '50%',
         backgroundColor: '#ffffff',
         mixBlendMode: 'difference',
