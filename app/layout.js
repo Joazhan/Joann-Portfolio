fb0d9c267985e80cda757b1f8536a2d992442868
Joazhan <joannzhang4@gmail.com>
2026-05-05 01:45:33 -0700

    layout: suppressHydrationWarning on body to silence Grammarly extension mismatch

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={geist.className} style={{ overflowX: 'hidden' }}>
+      <body className={geist.className} style={{ overflowX: 'hidden' }} suppressHydrationWarning>
         <CustomCursor />
         <Navbar />
         {children}
